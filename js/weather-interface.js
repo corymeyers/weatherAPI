959e453133df54bfc081b31497e72c0366c9f00f
cory meyers and Elissa Sosland <elissa@>
2016-05-25 11:00:50 -0700

    finish weather API, refactor cod to add fail method

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,13 +1,14 @@
-var apiKey = "=e0c3d32e86b53627e0515aefe68711a3";
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is" + city + ".");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid' + apiKey, function(response){
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+        $('.showWeather2').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    }).fail(function(error) {
+    $('.showWeather').text(error.responseJSON.message);
     });
-    console.log("Notice: The GET request has been made.");
   });
 });
